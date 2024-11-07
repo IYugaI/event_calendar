e12ca02aea2ae02d8cd74e281042f4c68bbfbf66
Oleksii <thamasomalley@gmail.com>
2024-11-07 22:50:17 +0100

    test slick slider on a separate html layout

---
+++ layout blueprint/js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.week-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: true,
+        infinite: true,
+        arrows: true
+    });
+});
