eb3c1b9b49eaceefcfb9bab467f73d2f94425449
Oleksii <thamasomalley@gmail.com>
2024-11-15 15:50:44 +0100

    order the files; create a template for navbar

--- day-details.php
+++ day-details.php
@@ -59,30 +59,7 @@ $events = mysqli_fetch_all($result, MYSQLI_ASSOC);
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Sport Calendar</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/home.php">Events</a>
-                        <a class="nav-link" href="#">Tickets</a>
-                        <a class="nav-link" href="#">About</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php require_once "./templates/navbar.php" ?>
 
     <main>
         <div class="outer-container">
--- home.php
+++ home.php
@@ -61,34 +61,11 @@ $days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Event Calendar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="./custom_slider/styles.css">
+    <link rel="stylesheet" href="./styles/slider.css">
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Sport Calendar</a>
-                <button
-                    class="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/home.php">Events</a>
-                        <a class="nav-link" href="#">Tickets</a>
-                        <a class="nav-link" href="#">About</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php require_once "./templates/navbar.php" ?>
 
     <main>
         <div class="slider">
@@ -102,7 +79,7 @@ $days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturd
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="./custom_slider/index.js"></script>
+    <script src="./js/slider.js"></script>
 </body>
 
 </html>
